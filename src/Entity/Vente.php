ac210ae3686f1952086813a4513ac739790ac334
jaoiad <51856957+jaoiad@users.noreply.github.com>
2020-10-16 12:05:45 +0200

    16.10.20 12h00

---
+++ src/Entity/Vente.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VenteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=VenteRepository::class)
+ */
+class Vente
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $creat_at;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $denomination;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $categorie;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $surface;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type_maison;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $chambres;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $etage;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $cout;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $accessibilite;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatAt(): ?\DateTimeInterface
+    {
+        return $this->creat_at;
+    }
+
+    public function setCreatAt(\DateTimeInterface $creat_at): self
+    {
+        $this->creat_at = $creat_at;
+
+        return $this;
+    }
+
+    public function getDenomination(): ?string
+    {
+        return $this->denomination;
+    }
+
+    public function setDenomination(string $denomination): self
+    {
+        $this->denomination = $denomination;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?string
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(string $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getSurface(): ?string
+    {
+        return $this->surface;
+    }
+
+    public function setSurface(string $surface): self
+    {
+        $this->surface = $surface;
+
+        return $this;
+    }
+
+    public function getTypeMaison(): ?string
+    {
+        return $this->type_maison;
+    }
+
+    public function setTypeMaison(string $type_maison): self
+    {
+        $this->type_maison = $type_maison;
+
+        return $this;
+    }
+
+    public function getChambres(): ?int
+    {
+        return $this->chambres;
+    }
+
+    public function setChambres(int $chambres): self
+    {
+        $this->chambres = $chambres;
+
+        return $this;
+    }
+
+    public function getEtage(): ?int
+    {
+        return $this->etage;
+    }
+
+    public function setEtage(int $etage): self
+    {
+        $this->etage = $etage;
+
+        return $this;
+    }
+
+    public function getCout(): ?int
+    {
+        return $this->cout;
+    }
+
+    public function setCout(int $cout): self
+    {
+        $this->cout = $cout;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getAccessibilite(): ?string
+    {
+        return $this->accessibilite;
+    }
+
+    public function setAccessibilite(string $accessibilite): self
+    {
+        $this->accessibilite = $accessibilite;
+
+        return $this;
+    }
+}
